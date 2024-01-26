35c9d7c4d27757a1cb30a5ea2eab3eba7bc3b103
HS-hyeokjin <youngurwls@gmail.com>
2024-01-26 13:23:47 +0900

    refactor : 코드수정

--- src/main/resources/script/schema.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE `member` (
-                          `member_id`	varchar(20)	NOT NULL,
-                          `email`	varchar(30)	NULL,
-                          `password`	varchar(30)	NULL,
-                          `status`	varchar(10)	NULL
-                          primary key (`member_id`)
-);
