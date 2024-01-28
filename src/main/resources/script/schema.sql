402a8eb84ae2401a410bb557d60502c9f9d6861c
Imheroman <duddnd4457@naver.com>
2024-01-28 21:58:20 +0900

    [제출] erd, sql 업로드

---
+++ src/main/resources/script/schema.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE `member` (
+	`member_id`	varchar(20)	NOT NULL,
+	`email`	varchar(30)	NULL,
+	`password`	varchar(30)	NULL,
+	`member_status`	varchar(10)	NULL
+);
+
+ALTER TABLE `member` ADD CONSTRAINT `PK_MEMBER` PRIMARY KEY (
+	`member_id`
+);
+
